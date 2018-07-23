3571d45b02c32e2b061f515bd81cf6477792dc09
Thomas Wirum Larsen <wirum@telefactory.no>
2018-07-24 00:34:49 +0200

    dont't remember. Comment of the year

--- zicbot.js
+++ zicbot.js
@@ -1,5 +1,5 @@
 /* let env = process.env.NODE_ENV || 'production';*/
-let env = process.env.NODE_ENV || 'development'
+let env = process.env.NODE_ENV || 'production'
 
 let config = require('./config')[env];
 let client = require('coffea')(config.IRC_OPTIONS)
