1cd64be49f2ecca0f68c75e1b6ad74a8b985d0db
Thomas Wirum Larsen <wirum@telefactory.no>
2018-07-23 15:57:30 +0200

    Reply like a dick when args are lacking or things are otherwise not nice

--- zicbot.js
+++ zicbot.js
@@ -1,5 +1,5 @@
 /* let env = process.env.NODE_ENV || 'production';*/
-let env = process.env.NODE_ENV || 'development'
+let env = process.env.NODE_ENV || 'production'
 
 let config = require('./config')[env];
 let client = require('coffea')(config.IRC_OPTIONS)
@@ -29,7 +29,7 @@ client.on('message', function (event) {
         thr = 0;
     }
     console.log(event.channel.name, event.user.nick, event.message)
-    
+
     // TODO: Make this less retarded. We wanna randomize how often we're being annoying somehow
     getRandomInt(2) >= 0.5 ? thr++ : thr
 });
@@ -38,7 +38,7 @@ function postMitsuku(event) {
     let m = require('mitsuku-api')();
             m.send(event.message.replace(config.IRC_OPTIONS.nick, ''))
                 .then(function (response) {
-                    event.reply(response.split('uku:').pop())
+                    response.length > 4 ? event.reply(response.split('uku:').pop()) : event.reply(config.DICK_REPLY)
                 });
 }
 
@@ -49,8 +49,11 @@ function postGoogleLink(event, wild = 2) {
         if (error) event.reply(config.ERROR_MSG + " " + error)
         for (let i = 0; i < wild; ++i) {
             let link = res.links[getRandomInt(wild)];
-            if (link.href) {
+            if (link && link.href) {
                 event.reply(link.href);
+                break;
+            } else {
+                event.reply(config.DICK_REPLY)
                 break;
             }
         }
@@ -58,24 +61,19 @@ function postGoogleLink(event, wild = 2) {
 }
 
 function postHsData(event) {
-    let str = ""
     let Request = require("request")
     Request.get(config.URL + config.USERNAME + config.TOKEN, (error, response, body) => {
         let total = JSON.parse(body).history.length
         if (error) {
             event.reply(config.ERROR_MSG + " " + error)
         }
-        str = ("<< Wirum HS standings! (again) >> \nCurrent rank: "
+        event.reply("<< Wirum HS standings! (again) >> \nCurrent rank: "
             + getRank(JSON.parse(body))
             + ", Current win percentage: "
             + getWinRatio(JSON.parse(body), total)
             + "%, Games played: "
             + total)
-        event.reply(str)
     });
-    return function(event) {
-        return str
-    }
 }
 
 function getWinRatio(data, total) {
